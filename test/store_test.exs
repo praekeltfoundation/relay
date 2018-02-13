528daaed90981d01ec52011cccaeda8eadb8e824
Jamie Hewland <jhewland@gmail.com>
2018-02-13 12:51:59 +0200

    Add an assertion that old resources do not invalidate newer ones

--- test/store_test.exs
+++ test/store_test.exs
@@ -28,6 +28,11 @@ defmodule Relay.StoreTest do
     # ...and we receive the notification
     assert_receive {:lds, "2", ^resources2}, 1_000
 
+    # ...if we try update with older resources
+    assert Store.update_lds(store, "1", resources) == :ok
+    # we receive no message and the state is unchanged
+    assert %Resources{version_info: "2", resources: ^resources2} = get_resources(store, :lds)
+
     # We can unsubscribe
     assert Store.unsubscribe_lds(store, self()) == :ok
 
@@ -52,6 +57,11 @@ defmodule Relay.StoreTest do
 
     # ...and we receive the notification
     assert_receive {:rds, "2", ^resources2}, 1_000
+
+    # ...if we try update with older resources
+    assert Store.update_rds(store, "1", resources) == :ok
+    # we receive no message and the state is unchanged
+    assert %Resources{version_info: "2", resources: ^resources2} = get_resources(store, :rds)
 
     # We can unsubscribe
     assert Store.unsubscribe_rds(store, self()) == :ok
@@ -78,6 +88,11 @@ defmodule Relay.StoreTest do
     # ...and we receive the notification
     assert_receive {:cds, "2", ^resources2}, 1_000
 
+    # ...if we try update with older resources
+    assert Store.update_cds(store, "1", resources) == :ok
+    # we receive no message and the state is unchanged
+    assert %Resources{version_info: "2", resources: ^resources2} = get_resources(store, :cds)
+
     # We can unsubscribe
     assert Store.unsubscribe_cds(store, self()) == :ok
 
@@ -102,6 +117,11 @@ defmodule Relay.StoreTest do
 
     # ...and we receive the notification
     assert_receive {:eds, "2", ^resources2}, 1_000
+
+    # ...if we try update with older resources
+    assert Store.update_eds(store, "1", resources) == :ok
+    # we receive no message and the state is unchanged
+    assert %Resources{version_info: "2", resources: ^resources2} = get_resources(store, :eds)
 
     # We can unsubscribe
     assert Store.unsubscribe_eds(store, self()) == :ok
