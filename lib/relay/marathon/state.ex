b7b3cbaefc93fa65a8a91566b93d88579f6121cb
Jamie Hewland <jhewland@gmail.com>
2018-03-01 13:50:46 +0200

    Relay.Marathon.State: typespec

--- lib/relay/marathon/state.ex
+++ lib/relay/marathon/state.ex
@@ -2,6 +2,11 @@ defmodule Relay.Marathon.State do
   alias Relay.Marathon.{App, Task}
 
   defstruct apps: %{}, tasks: %{}, app_tasks: %{}
+  @type t :: %__MODULE__{
+    apps: %{optional(String.t) => App.t},
+    tasks: %{optional(String.t) => Task.t},
+    app_tasks: %{optional(String.t) => String.t}
+  }
 
   @doc """
   Add an app to `state`. The app is only added if its version is newer than
@@ -11,6 +16,7 @@ defmodule Relay.Marathon.State do
   the state (or `nil` if there was no app with the ID) and the second element
   is the new state.
   """
+  @spec put_app(t, App.t) :: {String.t | nil, t}
   def put_app(%__MODULE__{apps: apps} = state, %App{id: id, version: version} = app) do
     case Map.get(apps, id) do
       # App is newer than existing app, update the app
@@ -27,9 +33,11 @@ defmodule Relay.Marathon.State do
     end
   end
 
+  @spec add_app(t, App.t) :: t
   defp add_app(%__MODULE__{apps: apps, app_tasks: app_tasks} = state, %App{id: id} = app),
     do: %{state | apps: Map.put(apps, id, app), app_tasks: Map.put(app_tasks, id, MapSet.new())}
 
+  @spec update_app!(t, App.t) :: t
   defp update_app!(%__MODULE__{apps: apps} = state, %App{id: id} = app),
     do: %{state | apps: Map.replace!(apps, id, app)}
 
@@ -39,6 +47,7 @@ defmodule Relay.Marathon.State do
   Returns the new state. If the app is not in `state`, returns `state`
   unchanged.
   """
+  @spec delete_app(t, App.t) :: t
   def delete_app(%__MODULE__{apps: apps, tasks: tasks, app_tasks: app_tasks} = state, %App{id: id}) do
     case Map.pop(apps, id) do
       {%App{}, new_apps} ->
@@ -62,6 +71,7 @@ defmodule Relay.Marathon.State do
 
   Raises a `KeyError` if the app that the task belongs to is not in `state`.
   """
+  @spec put_task!(t, Task.t) :: {String.t | nil, t}
   def put_task!(%__MODULE__{tasks: tasks} = state, %Task{id: id, version: version} = task) do
     case Map.get(tasks, id) do
       # Task is newer than existing task, update the task
@@ -78,6 +88,7 @@ defmodule Relay.Marathon.State do
     end
   end
 
+  @spec add_task!(t, Task.t) :: t
   defp add_task!(
          %__MODULE__{tasks: tasks, app_tasks: app_tasks} = state,
          %Task{id: id, app_id: app_id} = task
@@ -89,6 +100,7 @@ defmodule Relay.Marathon.State do
     }
   end
 
+  @spec update_task!(t, Task.t) :: t
   defp update_task!(%__MODULE__{tasks: tasks} = state, %Task{id: id} = task),
     do: %{state | tasks: Map.replace!(tasks, id, task)}
 
@@ -100,6 +112,7 @@ defmodule Relay.Marathon.State do
 
   Raises a `KeyError` if the app that the task belongs to is not in `state`.
   """
+  @spec delete_task!(t, Task.t) :: t
   def delete_task!(%__MODULE__{tasks: tasks, app_tasks: app_tasks} = state, %Task{
         id: id,
         app_id: app_id
