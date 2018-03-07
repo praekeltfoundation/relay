53d063c56bb6aa9c6ec0d29253e58ea622c409dc
Jamie Hewland <jhewland@gmail.com>
2018-03-07 11:55:50 +0200

    Run mix format on Relay.Store

--- lib/relay/store.ex
+++ lib/relay/store.ex
@@ -8,7 +8,6 @@ defmodule Relay.Store do
 
   @type discovery_service :: :lds | :rds | :cds | :eds
 
-
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
@@ -17,26 +16,24 @@ defmodule Relay.Store do
     defstruct version_info: "", resources: [], subscribers: MapSet.new()
   end
 
-
   ## Client interface
 
   defguardp is_xds(xds) when xds in @discovery_services
 
   @spec subscribe(identifier | atom, discovery_service, pid) :: :ok
-  def subscribe(server, xds, pid) when is_xds(xds), do:
-    GenServer.call(server, {:subscribe, xds, pid})
+  def subscribe(server, xds, pid) when is_xds(xds),
+    do: GenServer.call(server, {:subscribe, xds, pid})
 
   @spec unsubscribe(identifier | atom, discovery_service, pid) :: :ok
-  def unsubscribe(server, xds, pid) when is_xds(xds), do:
-    GenServer.call(server, {:unsubscribe, xds, pid})
+  def unsubscribe(server, xds, pid) when is_xds(xds),
+    do: GenServer.call(server, {:unsubscribe, xds, pid})
 
   @spec update(identifier | atom, :lds, String.t, [Envoy.Api.V2.Listener.t]) :: :ok
   @spec update(identifier | atom, :rds, String.t, [Envoy.Api.V2.RouteConfiguration.t]) :: :ok
   @spec update(identifier | atom, :cds, String.t, [Envoy.Api.V2.Cluster.t]) :: :ok
   @spec update(identifier | atom, :eds, String.t, [Envoy.Api.V2.ClusterLoadAssignment.t]) :: :ok
-  def update(server, xds, version_info, resources) when is_xds(xds), do:
-    GenServer.call(server, {:update, xds, version_info, resources})
-
+  def update(server, xds, version_info, resources) when is_xds(xds),
+    do: GenServer.call(server, {:update, xds, version_info, resources})
 
   ## Server callbacks
 
@@ -67,14 +64,17 @@ defmodule Relay.Store do
 
   defp update_impl(xds, version_info, resources, state) do
     xds_resources = Map.get(state, xds)
-    new_state = if xds_resources.version_info < version_info do
-      notify_subscribers(xds_resources.subscribers, xds, version_info, resources)
 
-      new_resources = %{xds_resources | version_info: version_info, resources: resources}
-      Map.put(state, xds, new_resources)
-    else
-      state
-    end
+    new_state =
+      if xds_resources.version_info < version_info do
+        notify_subscribers(xds_resources.subscribers, xds, version_info, resources)
+
+        new_resources = %{xds_resources | version_info: version_info, resources: resources}
+        Map.put(state, xds, new_resources)
+      else
+        state
+      end
+
     {:reply, :ok, new_state}
   end
 
@@ -84,16 +84,14 @@ defmodule Relay.Store do
   defp notify_subscriber(subscriber, xds, version_info, resources),
     do: send(subscriber, {xds, version_info, resources})
 
-  def handle_call({:subscribe, xds, pid}, _from, state), do:
-    subscribe_impl(xds, pid, state)
+  def handle_call({:subscribe, xds, pid}, _from, state), do: subscribe_impl(xds, pid, state)
 
-  def handle_call({:unsubscribe, xds, pid}, _from, state), do:
-    unsubscribe_impl(xds, pid, state)
+  def handle_call({:unsubscribe, xds, pid}, _from, state), do: unsubscribe_impl(xds, pid, state)
 
-  def handle_call({:update, xds, version_info, resources}, _from, state), do:
-    update_impl(xds, version_info, resources, state)
+  def handle_call({:update, xds, version_info, resources}, _from, state),
+    do: update_impl(xds, version_info, resources, state)
 
   # For testing only
-  def handle_call({:_get_resources, xds}, _from, state), do:
-    {:reply, {:ok, Map.get(state, xds)}, state}
+  def handle_call({:_get_resources, xds}, _from, state),
+    do: {:reply, {:ok, Map.get(state, xds)}, state}
 end
