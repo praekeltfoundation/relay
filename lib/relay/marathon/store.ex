16a96ff6c529190ea00eec486f715dc147fb49e2
Jamie Hewland <jhewland@gmail.com>
2018-03-09 12:51:40 +0200

    Move state mutating functions to state module

--- lib/relay/marathon/store.ex
+++ lib/relay/marathon/store.ex
@@ -11,8 +11,94 @@ defmodule Relay.Marathon.Store do
       tasks: %{optional(String.t) => Task.t},
       app_tasks: %{optional(String.t) => String.t}
     }
+
+    @spec get_and_update_app(t, App.t) :: {App.t | nil, t}
+    def get_and_update_app(%__MODULE__{apps: apps} = state, %App{id: id, version: version} = app) do
+      case Map.get(apps, id) do
+        # App is newer than existing app, update the app
+        %App{version: existing_version} = existing_app when version > existing_version ->
+          {existing_app, replace_app!(state, app)}
+
+        # App is the same or older than existing app, do nothing
+        %App{} ->
+          {app, state}
+
+        # No existing app with this ID, add this one
+        nil ->
+          {nil, put_app(state, app)}
+      end
+    end
+
+    @spec put_app(t, App.t) :: t
+    defp put_app(%__MODULE__{apps: apps, app_tasks: app_tasks} = state, %App{id: id} = app),
+      do: %{state | apps: Map.put(apps, id, app), app_tasks: Map.put(app_tasks, id, MapSet.new())}
+
+    @spec replace_app!(t, App.t) :: t
+    defp replace_app!(%__MODULE__{apps: apps} = state, %App{id: id} = app),
+      do: %{state | apps: Map.replace!(apps, id, app)}
+
+    @spec pop_app(t, String.t) :: {App.t | nil, t}
+    def pop_app(%__MODULE__{apps: apps, tasks: tasks, app_tasks: app_tasks} = state, id) do
+      case Map.pop(apps, id) do
+        {%App{} = app, new_apps} ->
+          {tasks_for_app, new_app_tasks} = Map.pop(app_tasks, id)
+          new_tasks = Map.drop(tasks, tasks_for_app)
+
+          {app, %{state | apps: new_apps, tasks: new_tasks, app_tasks: new_app_tasks}}
+
+        {nil, _} ->
+          {nil, state}
+      end
+    end
+
+    @spec get_and_update_task!(t, Task.t) :: {Task.t | nil, t}
+    def get_and_update_task!(%__MODULE__{tasks: tasks} = state, %Task{id: id, version: version} = task) do
+      case Map.get(tasks, id) do
+        # Task is newer than existing task, update the task
+        %Task{version: existing_version} = existing_task when version > existing_version ->
+          {existing_task, replace_task!(state, task)}
+
+        # Task is the same or older than existing task, do nothing
+        %Task{} ->
+          {task, state}
+
+        # No existing task with this ID, add this one
+        nil ->
+          {nil, put_task!(state, task)}
+      end
+    end
+
+    @spec put_task!(t, Task.t) :: t
+    defp put_task!(
+           %__MODULE__{tasks: tasks, app_tasks: app_tasks} = state,
+           %Task{id: id, app_id: app_id} = task
+         ) do
+      %{
+        state
+        | tasks: Map.put(tasks, id, task),
+          app_tasks: Map.update!(app_tasks, app_id, fn tasks -> MapSet.put(tasks, id) end)
+      }
+    end
+
+    @spec replace_task!(t, Task.t) :: t
+    defp replace_task!(%__MODULE__{tasks: tasks} = state, %Task{id: id} = task),
+      do: %{state | tasks: Map.replace!(tasks, id, task)}
+
+    @spec pop_task(t, String.t) :: {Task.t | nil, t}
+    def pop_task(%__MODULE__{tasks: tasks, app_tasks: app_tasks} = state, id) do
+      case Map.pop(tasks, id) do
+        {%Task{app_id: app_id} = task, new_tasks} ->
+          new_app_tasks = Map.update!(app_tasks, app_id, &MapSet.delete(&1, id))
+
+          {task, %{state | tasks: new_tasks, app_tasks: new_app_tasks}}
+
+        {nil, _} ->
+          {nil, state}
+      end
+    end
   end
 
+  @spec start_link(keyword) :: GenServer.on_start
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
@@ -48,7 +134,7 @@ defmodule Relay.Marathon.Store do
   end
 
   def handle_call({:update_app, %App{id: id, version: version} = app}, _from, state) do
-    {old_app, new_state} = get_and_update_app(state, app)
+    {old_app, new_state} = State.get_and_update_app(state, app)
     _ =
       case old_app do
         %App{version: existing_version} when version > existing_version  ->
@@ -67,7 +153,7 @@ defmodule Relay.Marathon.Store do
   end
 
   def handle_call({:delete_app, id}, _from, state) do
-    {app, new_state} = pop_app(state, id)
+    {app, new_state} = State.pop_app(state, id)
     _ =
       case app do
         %App{version: version} ->
@@ -84,7 +170,7 @@ defmodule Relay.Marathon.Store do
   def handle_call({:update_task, %Task{id: id, app_id: app_id, version: version} = task}, _from, state) do
     new_state =
       try do
-        {old_task, new_state} = get_and_update_task!(state, task)
+        {old_task, new_state} = State.get_and_update_task!(state, task)
         _ =
           case old_task do
             %Task{version: existing_version} when version > existing_version  ->
@@ -110,7 +196,7 @@ defmodule Relay.Marathon.Store do
   end
 
   def handle_call({:delete_task, id}, _from, state) do
-    {task, new_state} = pop_task(state, id)
+    {task, new_state} = State.pop_task(state, id)
     _ =
       case task do
         %Task{version: version} ->
@@ -126,89 +212,4 @@ defmodule Relay.Marathon.Store do
 
   # For testing only
   def handle_call(:_get_state, _from, state), do: {:reply, {:ok, state}, state}
-
-  @spec get_and_update_app(State.t, App.t) :: {App.t | nil, State.t}
-  defp get_and_update_app(%State{apps: apps} = state, %App{id: id, version: version} = app) do
-    case Map.get(apps, id) do
-      # App is newer than existing app, update the app
-      %App{version: existing_version} = existing_app when version > existing_version ->
-        {existing_app, replace_app!(state, app)}
-
-      # App is the same or older than existing app, do nothing
-      %App{} ->
-        {app, state}
-
-      # No existing app with this ID, add this one
-      nil ->
-        {nil, put_app(state, app)}
-    end
-  end
-
-  @spec put_app(State.t, App.t) :: State.t
-  defp put_app(%State{apps: apps, app_tasks: app_tasks} = state, %App{id: id} = app),
-    do: %{state | apps: Map.put(apps, id, app), app_tasks: Map.put(app_tasks, id, MapSet.new())}
-
-  @spec replace_app!(State.t, App.t) :: State.t
-  defp replace_app!(%State{apps: apps} = state, %App{id: id} = app),
-    do: %{state | apps: Map.replace!(apps, id, app)}
-
-  @spec pop_app(State.t, String.t) :: {App.t | nil, State.t}
-  defp pop_app(%State{apps: apps, tasks: tasks, app_tasks: app_tasks} = state, id) do
-    case Map.pop(apps, id) do
-      {%App{} = app, new_apps} ->
-        {tasks_for_app, new_app_tasks} = Map.pop(app_tasks, id)
-        new_tasks = Map.drop(tasks, tasks_for_app)
-
-        {app, %{state | apps: new_apps, tasks: new_tasks, app_tasks: new_app_tasks}}
-
-      {nil, _} ->
-        {nil, state}
-    end
-  end
-
-  @spec get_and_update_task!(State.t, Task.t) :: {Task.t | nil, State.t}
-  defp get_and_update_task!(%State{tasks: tasks} = state, %Task{id: id, version: version} = task) do
-    case Map.get(tasks, id) do
-      # Task is newer than existing task, update the task
-      %Task{version: existing_version} = existing_task when version > existing_version ->
-        {existing_task, replace_task!(state, task)}
-
-      # Task is the same or older than existing task, do nothing
-      %Task{} ->
-        {task, state}
-
-      # No existing task with this ID, add this one
-      nil ->
-        {nil, put_task!(state, task)}
-    end
-  end
-
-  @spec put_task!(State.t, Task.t) :: State.t
-  defp put_task!(
-         %State{tasks: tasks, app_tasks: app_tasks} = state,
-         %Task{id: id, app_id: app_id} = task
-       ) do
-    %{
-      state
-      | tasks: Map.put(tasks, id, task),
-        app_tasks: Map.update!(app_tasks, app_id, fn tasks -> MapSet.put(tasks, id) end)
-    }
-  end
-
-  @spec replace_task!(State.t, Task.t) :: State.t
-  defp replace_task!(%State{tasks: tasks} = state, %Task{id: id} = task),
-    do: %{state | tasks: Map.replace!(tasks, id, task)}
-
-  @spec pop_task(State.t, String.t) :: {Task.t | nil, State.t}
-  defp pop_task(%State{tasks: tasks, app_tasks: app_tasks} = state, id) do
-    case Map.pop(tasks, id) do
-      {%Task{app_id: app_id} = task, new_tasks} ->
-        new_app_tasks = Map.update!(app_tasks, app_id, &MapSet.delete(&1, id))
-
-        {task, %{state | tasks: new_tasks, app_tasks: new_app_tasks}}
-
-      {nil, _} ->
-        {nil, state}
-    end
-  end
 end
