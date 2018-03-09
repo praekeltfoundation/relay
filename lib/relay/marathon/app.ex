3006063d21eb5558c82a41496d4d23d35d3fc901
Jamie Hewland <jhewland@gmail.com>
2018-03-09 16:16:07 +0200

    mix format on Marathon.App

--- lib/relay/marathon/app.ex
+++ lib/relay/marathon/app.ex
@@ -2,18 +2,20 @@ defmodule Relay.Marathon.App do
   alias Relay.Marathon.{Labels, Networking}
 
   defstruct [:id, :networking_mode, :ports_list, :port_indices_in_group, :labels, :version]
-  @type t :: %__MODULE__{
-    id: String.t,
-    networking_mode: Networking.networking_mode,
-    ports_list: [Networking.port_number],
-    port_indices_in_group: [non_neg_integer],
-    labels: Labels.labels,
-    version: String.t
-  }
 
-  @spec from_definition(map, String.t) :: t
+  @type t :: %__MODULE__{
+          id: String.t(),
+          networking_mode: Networking.networking_mode(),
+          ports_list: [Networking.port_number()],
+          port_indices_in_group: [non_neg_integer],
+          labels: Labels.labels(),
+          version: String.t()
+        }
+
+  @spec from_definition(map, String.t()) :: t
   def from_definition(%{"id" => id, "labels" => labels} = app, group) do
     ports_list = Networking.ports_list(app)
+
     %__MODULE__{
       id: id,
       networking_mode: Networking.networking_mode(app),
@@ -24,13 +26,17 @@ defmodule Relay.Marathon.App do
     }
   end
 
-  @spec from_event(map, String.t) :: t
-  def from_event(%{"eventType" => "api_post_event", "appDefinition" => definition} = _event, group),
-    do: from_definition(definition, group)
+  @spec from_event(map, String.t()) :: t
+  def from_event(
+        %{"eventType" => "api_post_event", "appDefinition" => definition} = _event,
+        group
+      ),
+      do: from_definition(definition, group)
 
   defp port_indices_in_group([], _labels, _group), do: []
 
-  @spec port_indices_in_group([Networking.port_number], %{String.t => String.t}, String.t) :: [non_neg_integer]
+  @spec port_indices_in_group([Networking.port_number()], %{String.t() => String.t()}, String.t()) ::
+          [non_neg_integer]
   defp port_indices_in_group(ports_list, labels, group) do
     0..(length(ports_list) - 1)
     |> Enum.filter(fn port_index -> Labels.marathon_lb_group(labels, port_index) == group end)
@@ -40,13 +46,16 @@ defmodule Relay.Marathon.App do
   defp version(app) do
     case app do
       # In most cases the `lastConfigChangeAt` value should be available...
-      %{"versionInfo" => %{"lastConfigChangeAt" => version}} -> version
+      %{"versionInfo" => %{"lastConfigChangeAt" => version}} ->
+        version
+
       # ...but if this is an app that hasn't been changed yet then use `version`
-      %{"version" => version} -> version
+      %{"version" => version} ->
+        version
     end
   end
 
-  @spec marathon_lb_vhost(t, non_neg_integer) :: [String.t]
+  @spec marathon_lb_vhost(t, non_neg_integer) :: [String.t()]
   def marathon_lb_vhost(%__MODULE__{labels: labels}, port_index),
     do: Labels.marathon_lb_vhost(labels, port_index)
 
@@ -54,7 +63,7 @@ defmodule Relay.Marathon.App do
   def marathon_lb_redirect_to_https?(%__MODULE__{labels: labels}, port_index),
     do: Labels.marathon_lb_redirect_to_https?(labels, port_index)
 
-  @spec marathon_acme_domain(t, non_neg_integer) :: [String.t]
+  @spec marathon_acme_domain(t, non_neg_integer) :: [String.t()]
   def marathon_acme_domain(%__MODULE__{labels: labels}, port_index),
     do: Labels.marathon_acme_domain(labels, port_index)
 end
