84299a5b82322ba04f4050a1a7031fde6f79174b
Jeremy Thurgood <jeremy@praekelt.org>
2018-03-13 14:52:05 +0200

    More cert utils.

--- lib/relay/certs.ex
+++ lib/relay/certs.ex
@@ -1,9 +1,22 @@
 defmodule Relay.Certs do
 
+  @typep pem_entry :: :public_key.pem_entry
+
   # This is a somewhat loose regex designed to exclude things that obviously
   # aren't hostnames. It will allow some non-hostnames, because full validation
   # would be a lot more complex.
   @hostname_regex ~r/^[a-zA-Z0-9.-]+$/
+
+  @key_types [:RSAPrivateKey, :DSAPrivateKey, :ECPrivateKey]
+
+  @doc """
+  Encodes the given PEM data (either a single entry, a list of entries, or an
+  already-encoded binary) in PEM format.
+  """
+  @spec pem_encode(binary | pem_entry | [pem_entry]) :: binary
+  def pem_encode(pem_data) when is_binary(pem_data), do: pem_data
+  def pem_encode(pem_data = {_, _, _}), do: pem_encode([pem_data])
+  def pem_encode(pem_data), do: :public_key.pem_encode(pem_data)
 
   @doc """
   Extracts the subject CNs and SAN DNS names from the given certificate to
@@ -18,28 +31,42 @@ defmodule Relay.Certs do
   end
 
   @doc """
+  Returns a list of all certificates in the given PEM data.
+  """
+  @spec get_certs(binary | [pem_entry]) :: [pem_entry]
+  def get_certs(pem_data) do
+    pem_decode(pem_data)
+    |> Enum.filter(fn {:Certificate, _, _} -> true; _ -> false end)
+  end
+
+  @doc """
+  Gets the first key in the given PEM data. Returns `{:ok, key}` if there is at
+  least one key, otherwise `:error`.
+  """
+  @spec get_key(binary | [pem_entry]) :: {:ok, pem_entry} | :error
+  def get_key(pem_data) do
+    pem_decode(pem_data)
+    |> Enum.filter(fn {key_type, _, _} -> key_type in @key_types end)
+    |> Enum.fetch(0)
+  end
+
+  defp pem_decode(pem_data) when is_list(pem_data), do: pem_data
+  defp pem_decode(pem_data), do: :public_key.pem_decode(pem_data)
+
+  @doc """
   Extracts hostnames from all end-entity certs in the given PEM data. In order
   to support self-signed certs (which may look a lot like CA certs), we assume
   that if there's only one cert in the PEM data it's the one we want.
   """
-  @spec get_end_entity_hostnames(binary | [:public_key.pem_entry]) :: [String.t]
-  def get_end_entity_hostnames(pem_data) when is_binary(pem_data),
-    do: get_end_entity_hostnames(:public_key.pem_decode(pem_data))
-
-  def get_end_entity_hostnames(pem_entries) do
-    pem_entries
-    |> filter_certs()
+  @spec get_end_entity_hostnames(binary | [pem_entry]) :: [String.t]
+  def get_end_entity_hostnames(pem_data) do
+    get_certs(pem_data)
+    |> get_end_entity_certs()
     |> Enum.flat_map(&get_hostnames/1)
-    |> Enum.uniq()
+    |> Enum.uniq
   end
 
-  defp filter_certs(pem_entries) do
-    case Enum.filter(pem_entries, &is_cert/1) do
-      [cert] -> [cert]
-      certs -> :relay_pk_utils.get_end_entity_certs(certs)
-    end
-  end
-
-  defp is_cert({:Certificate, _, _}), do: true
-  defp is_cert(_), do: false
+  defp get_end_entity_certs([cert]), do: [cert]
+  defp get_end_entity_certs(certs),
+    do: :relay_pk_utils.get_end_entity_certs(certs)
 end
--- test/relay/certs_test.exs
+++ test/relay/certs_test.exs
@@ -9,6 +9,32 @@ defmodule Relay.CertsTest do
     |> File.read!()
   end
 
+  test "pem encode" do
+    [key, cert, cacert] = :public_key.pem_decode(read_cert!("localhost.pem"))
+    pem_data = :public_key.pem_encode([key, cert, cacert])
+    assert ^pem_data = Certs.pem_encode(pem_data)
+    assert ^pem_data = Certs.pem_encode([key, cert, cacert])
+    pem_key = :public_key.pem_encode([key])
+    assert ^pem_key = Certs.pem_encode(key)
+  end
+
+  test "get certs from pem data" do
+    pem_data = read_cert!("localhost.pem")
+    [_key, cert, cacert] = :public_key.pem_decode(pem_data)
+    assert [^cert, ^cacert] = Certs.get_certs(pem_data)
+  end
+
+  test "get key from pem data" do
+    pem_data = read_cert!("localhost.pem")
+    [key, _cert, _cacert] = :public_key.pem_decode(pem_data)
+    assert {:ok, ^key} = Certs.get_key(pem_data)
+  end
+
+  test "get key from pem data with no keys" do
+    certs = Certs.get_certs(read_cert!("localhost.pem"))
+    assert :error = Certs.get_key(certs)
+  end
+
   test "get a list of valid hostnames from a cert" do
     [_key, cert, _cacert] = :public_key.pem_decode(read_cert!("localhost.pem"))
     hostnames = Certs.get_hostnames(cert)
