b82215b6bf51944c3565a57ca7bbbdf496eeb552
Jeremy Thurgood <jeremy@praekelt.org>
2018-03-14 12:15:40 +0200

    Certs test improvements.

--- test/relay/certs_test.exs
+++ test/relay/certs_test.exs
@@ -12,58 +12,58 @@ defmodule Relay.CertsTest do
   test "pem encode" do
     [key, cert, cacert] = :public_key.pem_decode(read_cert!("localhost.pem"))
     pem_data = :public_key.pem_encode([key, cert, cacert])
-    assert ^pem_data = Certs.pem_encode(pem_data)
-    assert ^pem_data = Certs.pem_encode([key, cert, cacert])
-    pem_key = :public_key.pem_encode([key])
-    assert ^pem_key = Certs.pem_encode(key)
+    assert Certs.pem_encode(pem_data) == pem_data
+    assert Certs.pem_encode([key, cert, cacert]) == pem_data
+    assert Certs.pem_encode(key) == :public_key.pem_encode([key])
   end
 
   test "get certs from pem data" do
     pem_data = read_cert!("localhost.pem")
     [_key, cert, cacert] = :public_key.pem_decode(pem_data)
-    assert [^cert, ^cacert] = Certs.get_certs(pem_data)
+    assert Certs.get_certs(pem_data) == [cert, cacert]
   end
 
   test "get key from pem data" do
     pem_data = read_cert!("localhost.pem")
     [key, _cert, _cacert] = :public_key.pem_decode(pem_data)
-    assert {:ok, ^key} = Certs.get_key(pem_data)
+    assert Certs.get_key(pem_data) == {:ok, key}
   end
 
   test "get key from pem data with no keys" do
     certs = Certs.get_certs(read_cert!("localhost.pem"))
-    assert :error = Certs.get_key(certs)
+    assert Certs.get_key(certs) == :error
   end
 
   test "get a list of valid hostnames from a cert" do
     [_key, cert, _cacert] = :public_key.pem_decode(read_cert!("localhost.pem"))
     hostnames = Certs.get_hostnames(cert)
-    assert ["localhost", "pebble"] = Enum.sort(hostnames)
+    assert Enum.sort(hostnames) == ["localhost", "pebble"]
   end
 
   test "some certs may have no hostnames" do
     [_key, _cert, cacert] = :public_key.pem_decode(read_cert!("localhost.pem"))
-    assert [] = Certs.get_hostnames(cacert)
+    assert Certs.get_hostnames(cacert) == []
   end
 
   test "collect hostnames for all end-entity certs in pem data" do
     hostnames = Certs.get_end_entity_hostnames(read_cert!("localhost.pem"))
-    assert ["localhost", "pebble"] = Enum.sort(hostnames)
+    assert Enum.sort(hostnames) == ["localhost", "pebble"]
   end
 
   test "collect hostnames for self-signed cert without SAN" do
     hostnames = Certs.get_end_entity_hostnames(read_cert!("demo-nosan.pem"))
-    assert ["demo-nosan.example.com"] = Enum.sort(hostnames)
+    assert Enum.sort(hostnames) == ["demo-nosan.example.com"]
   end
 
   test "collect hostnames for self-signed cert with SAN" do
     hostnames = Certs.get_end_entity_hostnames(read_cert!("demo.pem"))
-    assert ["demo.example.com", "demo.example.net"] = Enum.sort(hostnames)
+    assert Enum.sort(hostnames) == ["demo.example.com", "demo.example.net"]
   end
 
-  test ":relay_pk_utils.get_end_entity_certs" do
-    # This tests an edge case that isn't exercised through Relay.Certs.
+  test ":relay_pk_utils.get_end_entity_certs filters out non-certs" do
+    # This code path is never exercised through Relay.Certs, so we have to test
+    # it directly.
     [key, cert, cacert] = :public_key.pem_decode(read_cert!("localhost.pem"))
-    assert [^cert] = :relay_pk_utils.get_end_entity_certs([key, cert, cacert])
+    assert :relay_pk_utils.get_end_entity_certs([key, cert, cacert]) == [cert]
   end
 end
