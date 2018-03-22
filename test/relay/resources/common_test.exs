f72213ea313d3e9083920d1353988cb2b4f88f53
Jamie Hewland <jhewland@gmail.com>
2018-03-22 16:33:56 +0200

    Run mix format

--- test/relay/resources/common_test.exs
+++ test/relay/resources/common_test.exs
@@ -5,13 +5,13 @@ defmodule Relay.Resources.CommonTest do
 
   describe "truncate_obj_name/1" do
     test "long names truncated from beginning" do
-      TestHelpers.put_env(:relay, :envoy, [max_obj_name_length: 10])
+      TestHelpers.put_env(:relay, :envoy, max_obj_name_length: 10)
 
       assert Common.truncate_obj_name("helloworldmynameis") == "[...]ameis"
     end
 
     test "short names unchanged" do
-      TestHelpers.put_env(:relay, :envoy, [max_obj_name_length: 10])
+      TestHelpers.put_env(:relay, :envoy, max_obj_name_length: 10)
 
       assert Common.truncate_obj_name("hello") == "hello"
     end
