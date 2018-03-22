eca74780a0be2398790d6eef10818869c325ee95
Jamie Hewland <jhewland@gmail.com>
2018-03-22 12:11:58 +0200

    credo: Add some more config

--- config/.credo.exs
+++ config/.credo.exs
@@ -2,11 +2,19 @@
   configs: [
     %{
       name: "default",
+      color: true,
       files: %{
         included: ["config/", "lib/", "test/", "*.exs"],
         excluded: ["lib/envoy/", "lib/google/"]
       },
-      checks: []
+      checks: [
+        # Don't fail on TODOs and FIXMEs for now
+        {Credo.Check.Design.TagTODO, exit_status: 0},
+        {Credo.Check.Design.TagFIXME, exit_status: 0},
+
+        # mix format seems to enforce a line length of 100
+        {Credo.Check.Readability.MaxLineLength, max_length: 100},
+      ]
     }
   ]
 }
