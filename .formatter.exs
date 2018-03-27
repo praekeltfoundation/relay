1732e1a012acdeaa3483c9dd6493d194fb82e304
Jamie Hewland <jhewland@gmail.com>
2018-03-27 16:50:24 +0200

    Clean up .formatter.exs

--- .formatter.exs
+++ .formatter.exs
@@ -1,14 +1,10 @@
 # Used by "mix format"
 
-# The only way to adjust which files `mix format` considers is with the `input`
-# option. It is possible to have different .formatter.exs files per-directory,
-# but we'd rather not have config in many different files.
-
-include_patterns = ["*.exs", "{config,lib,test}/**/*.{ex,exs}"]
+# The default glob pattern matching doesn't match files starting with a '.'
+inputs = ["*.exs", "{config,lib,test}/**/*.{ex,exs}"] ++ Path.wildcard(".*.exs", match_dot: true)
 
 [
   import_deps: [:grpc, :protobuf],
-  # The default glob pattern matching doesn't match files starting with a '.'
-  inputs: include_patterns |> Enum.flat_map(&Path.wildcard(&1, match_dot: true)),
+  inputs: inputs,
   locals_without_parens: [xds_tests: 2]
 ]
